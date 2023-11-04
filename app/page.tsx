08355822b6638e030d07e84c22e21ec8c7020064
Mike Jonas <motion@gmail.com>
2023-11-03 20:58:23 -0700

    Dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,22 @@ export default function Home() {
     <div className="flex mt-4">
       <a
         href="https://github.com/mikejonas"
-        className="text-3xl mr-4 hover:text-gray-600 transition-colors"
+        className="text-3xl mr-4 hover:text-gray-300 transition-colors"
+        style={{ color: "inherit" }} // ensures the color inherits from the parent for easier theming
       >
         <FaGithub aria-label="GitHub" />
       </a>
       <a
         href="https://www.linkedin.com/in/mikejonas"
-        className="text-3xl mr-4 hover:text-gray-600 transition-colors"
+        className="text-3xl mr-4 hover:text-gray-300 transition-colors"
+        style={{ color: "inherit" }}
       >
         <FaLinkedin aria-label="LinkedIn" />
       </a>
       <a
         href="https://www.instagram.com/mikejonas_"
-        className="text-3xl hover:text-gray-600 transition-colors"
+        className="text-3xl hover:text-gray-300 transition-colors"
+        style={{ color: "inherit" }}
       >
         <FaInstagram aria-label="Instagram" />
       </a>
@@ -26,7 +29,7 @@ export default function Home() {
 
   const body = () => {
     return (
-      <p className="typewriter text-base font-normal max-w-prose">
+      <p className="typewriter text-base font-normal max-w-prose text-gray-300">
         <span>
           Passionate about building scalable software solutions and seamless
           user interfaces that drive business and user engagement.
@@ -37,10 +40,11 @@ export default function Home() {
   };
 
   return (
-    <div className="bg-white text-black min-h-screen flex flex-col  items-start px-10 py-20 space-y-6">
+    <div className="bg-zinc-950 text-gray-100 min-h-screen flex flex-col items-start px-10 py-20 space-y-6">
       <h1 className="text-5xl font-serif font-bold">Mike Jonas</h1>
       <p className="text-2xl font-normal">Software Engineer</p>
       {renderSocialIcons()}
+      {/* {body()} */}
     </div>
   );
 }
